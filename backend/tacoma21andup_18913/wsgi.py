2959a59911768db65c42d13671a5e80ab820071e
Crowdbotics <team@crowdbotics.com>
2020-07-15 08:36:07 +0000

    Update models

--- backend/tacoma21andup_18913/wsgi.py
+++ backend/tacoma21andup_18913/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tacoma21andup_18913.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tacoma21andup_18913.settings")
 
 application = get_wsgi_application()
